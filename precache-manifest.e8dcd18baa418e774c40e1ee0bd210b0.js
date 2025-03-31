d43e4eb58a86949b313ab6f0b2042cfb1732fa6e
vertraut <vertraut@users.noreply.github.com>
2025-03-31 17:50:32 +0000

    deploy: d1370807923a28468cb1f0b027cf7fd714b80c9f

---
+++ precache-manifest.e8dcd18baa418e774c40e1ee0bd210b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "accfbad6bc91053d4c43348217feb6ea",
+    "url": "https://vertraut.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "e2b58de6df298db01d06",
+    "url": "https://vertraut.github.io/task-management-frontend/static/css/main.8aed83bb.chunk.css"
+  },
+  {
+    "revision": "a2b831278b340fdec59d",
+    "url": "https://vertraut.github.io/task-management-frontend/static/js/2.c937a3cb.chunk.js"
+  },
+  {
+    "revision": "e2b58de6df298db01d06",
+    "url": "https://vertraut.github.io/task-management-frontend/static/js/main.4b00f444.chunk.js"
+  },
+  {
+    "revision": "a8fbba7fb67ed13b4762",
+    "url": "https://vertraut.github.io/task-management-frontend/static/js/runtime~main.9e543ad7.js"
+  }
+]);
